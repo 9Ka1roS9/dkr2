2601cf653729492310b1873759322a3078a06554
9Ka1roS9 <118186527+9Ka1roS9@users.noreply.github.com>
2022-12-15 02:50:05 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,16 @@
+﻿const n=5; 
+begin 
+var a: array[1 .. n] of integer; 
+var s, k : integer; 
+for var i:= 1 to n do begin 
+print (' a [', i ,'] = ' ); 
+readln (a[i]); 
+end; 
+s:=0; 
+print ('k='); 
+readln (k); 
+for var i:= 1 to n do 
+s:=s+a[i]; 
+if s>k then println ('Сумма элементов превосходит ', k) 
+else println ('Сумма элементов не превосходит ', k); 
+end. 
